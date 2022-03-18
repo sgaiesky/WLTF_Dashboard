0f5ffc7a9a2071e9d7be83f67b48ac6ea2e31626
sgaiesky <sgaiesky@gmail.com>
2022-03-18 14:01:53 +0000

    test commit

--- wltf_dashboard/app.R
+++ wltf_dashboard/app.R
@@ -1,19 +1,12 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(tidyverse)
+library(gsheet)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("West London Track & Field Athlete Monitering"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
