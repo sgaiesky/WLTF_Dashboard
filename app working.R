bcf93286d30b7df0f581018c71ec3c74e1eb57b4
sgaiesky <sgaiesky@gmail.com>
2022-03-19 12:26:25 +0000

    fixed date scale issues

--- app working.R
+++ app working.R
@@ -1,16 +1,21 @@
+rm(list = ls())
+
 library(tidyverse)
 library(gsheet)
 library(magrittr)
+library(lubridate)
 
-athlete <- c("Emmanuel", "Max")
-tests <- c("CMJ (mm)", "LCMJ (mm)")
-season <- c("21-22")
+athlete <- c("Max", "Emmanuel")
+tests <- c("CMJ (mm)", "LCMJ (mm)", "RCMJ (mm)")
+season <- c("21-22", "20-21", "19-20")
 
 url <- c("https://docs.google.com/spreadsheets/d/1fiRUwYv8FtysQQg7Z25Py-FW6KJeuMeoFCXuPmf89EE/edit?usp=sharing")
 dat <- gsheet::gsheet2tbl(url)
 
+dat$Date %<>% as.Date(format("%d/%m/%Y"))
 dat$Athlete %<>% as.factor()
 dat$Type %<>% as.factor()
+dat$Season %<>% as.factor()
 
 # %in% is used for multiple inputs
 dat <- dat %>%
@@ -21,6 +26,11 @@ dat <- dat %>%
 jump.graph <- ggplot(data = dat, mapping = aes(x = Date, y = Score)) +
   geom_point(mapping = aes(colour = Athlete, shape = Type)) +
   geom_line(mapping = aes(colour = Athlete, group = interaction(Athlete, Type))) +
+  scale_x_date(date_breaks = "1 month",
+               date_labels = "%B %Y") +
+  labs(y = "Height (mm)",
+       x = "Date") +
+  guides(x = guide_axis(angle = 90)) +
   theme_classic()
 
 print(jump.graph)
